8b594a1fceb3810d14594b0a5b21245023c3fd9c
hirof <hirofool502@gmail.com>
2024-05-18 00:22:04 +0300

    commit

--- business-logic/src/main/java/com/example/business/logic/controller/RegistryController.java
+++ business-logic/src/main/java/com/example/business/logic/controller/RegistryController.java
@@ -1,16 +1,26 @@
 package com.example.business.logic.controller;
 
 
+import com.example.business.logic.service.UserService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("main/flowers/create")
+@RequestMapping("main/registry")
 @Slf4j
 
 public class RegistryController {
 
+    private final UserService userService;
 
+    public RegistryController(UserService userService) {
+        this.userService = userService;
+    }
 
+//    public Long postUserByDTO(@Valid @ResponseBody ) {
+//
+//    }
 }
