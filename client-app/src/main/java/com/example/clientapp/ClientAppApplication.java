95c8e0c50638a2307c318eb8b9a730b927c1f712
hirof <hirofool502@gmail.com>
2024-05-17 20:02:09 +0300

    commit

--- client-app/src/main/java/com/example/clientapp/ClientAppApplication.java
+++ client-app/src/main/java/com/example/clientapp/ClientAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClientAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ClientAppApplication.class, args);
 	}
