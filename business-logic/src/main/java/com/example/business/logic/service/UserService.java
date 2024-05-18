e8065d8721aecb6d00c5894c75bb145391764197
hirof <hirofool502@gmail.com>
2024-05-18 11:17:53 +0300

    commit

--- business-logic/src/main/java/com/example/business/logic/service/UserService.java
+++ business-logic/src/main/java/com/example/business/logic/service/UserService.java
@@ -1,8 +1,10 @@
 package com.example.business.logic.service;
 
 
+import com.example.business.logic.exception.PasswordNotCorrectException;
 import com.example.business.logic.model.User;
 import com.example.business.logic.repository.UserRepository;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,13 +22,14 @@ public class UserService {
         return userRepository.findByLoginAndPassword(login, password);
     }
 
-    public Long CreateByLoginAndPassword(String login, String password) {
-
-        User user = new User();
-        user.setLogin(login);
-        user.setPassword(password);
-        return userRepository.save(user).getId();
-
+    public Long CreateByLoginAndPassword(String login, String password, String repeatPassword) {
+        if (password.equals(repeatPassword)) {
+            User user = new User();
+            user.setLogin(login);
+            user.setPassword(password);
+            return userRepository.save(user).getId();
+        }
+        throw new PasswordNotCorrectException("Passwords not equals");
     }
 
 }
