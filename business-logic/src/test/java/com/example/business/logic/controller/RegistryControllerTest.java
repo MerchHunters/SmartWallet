f90db94802bdb78f5849d9ad7385bc345be9799a
hirof <hirofool502@gmail.com>
2024-05-19 11:37:57 +0300

    there is two test

--- business-logic/src/test/java/com/example/business/logic/controller/RegistryControllerTest.java
+++ business-logic/src/test/java/com/example/business/logic/controller/RegistryControllerTest.java
@@ -3,6 +3,7 @@ package com.example.business.logic.controller;
 import com.example.business.logic.dto.RegistrationDataDTO;
 import com.example.business.logic.dto.ResponseDTO;
 import com.example.business.logic.model.Client;
+import com.example.business.logic.model.Wallet;
 import com.example.business.logic.repository.ClientRepository;
 import com.example.business.logic.repository.WalletRepository;
 import com.example.business.logic.service.ClientService;
@@ -19,7 +20,12 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 
@@ -55,7 +61,7 @@ public class RegistryControllerTest {
     }
 
     @Test
-    void postUserByDTO() {
+    void postUserByDTOTest() {
         ResponseDTO dataDTO = new ResponseDTO(200, 1234L);
         ResponseEntity<ResponseDTO> ok = ResponseEntity.ok(dataDTO);
 
@@ -65,6 +71,28 @@ public class RegistryControllerTest {
         ResponseEntity<?> result = registryController.postUserByDTO(registrationDataDTO);
 
         Assertions.assertEquals(ok, result);
+    }
+
+    @Test
+    void getAllWalletsTest() {
+
+        Client client = new Client();
+        client.setId(1234L);
+
+        List<Wallet> walletList = new ArrayList<>();
+        Wallet e = new Wallet();
+        e.setName("name1");
+        e.setDescription("first wallet");
+        walletList.add(e);
+        when(clientRepository.findFirstById(any())).thenReturn(client);
+        when(walletService.findByClientId(any())).thenReturn(walletList);
+
+        Collection<Wallet> allWallets = registryController.getAllWallets(12345L);
+
+
+        Assertions.assertEquals(walletList, allWallets);
+
+
     }
 
 
