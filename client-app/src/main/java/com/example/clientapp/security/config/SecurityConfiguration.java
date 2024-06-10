0351b534745ac8571bde05397474b214498b0bf9
Mr.D <danil36rusdanil@mail.ru>
2024-06-10 17:25:21 +0300

    add function to config

--- client-app/src/main/java/com/example/clientapp/security/config/SecurityConfiguration.java
+++ client-app/src/main/java/com/example/clientapp/security/config/SecurityConfiguration.java
@@ -1,20 +1,29 @@
 package com.example.clientapp.security.config;
 
 
+import com.example.clientapp.security.service.MyUserDetailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
 
+    private final MyUserDetailService myUserDetailService;
+    public final PasswordEncoder passwordEncoder;
+    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http.csrf(AbstractHttpConfigurer::disable)
@@ -24,9 +33,18 @@ public class SecurityConfiguration {
         })
                 .build();
     }
+    @Bean
+   public UserDetailsService userDetailsService(){
+        return myUserDetailService;
+    }
+
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(myUserDetailService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
     }
+
 }
