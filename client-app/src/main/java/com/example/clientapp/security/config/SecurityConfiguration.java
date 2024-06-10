27062166c85423a4ff558a69b72d19f5c71d2c56
Mr.D <danil36rusdanil@mail.ru>
2024-06-10 16:53:35 +0300

    connected the necessary ones depending Two

---
+++ client-app/src/main/java/com/example/clientapp/security/config/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.clientapp.security.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWeb
+public class SecurityConfiguration {
+}
