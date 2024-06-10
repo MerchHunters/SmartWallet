aa119820945ad62ec7c5d20601f3122f81fbf699
Mr.D <danil36rusdanil@mail.ru>
2024-06-10 17:20:24 +0300

    add Service

---
+++ client-app/src/main/java/com/example/clientapp/security/service/MyUserDetailService.java
@@ -0,0 +1,26 @@
+package com.example.clientapp.security.service;
+
+import com.example.clientapp.security.model.MyUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MyUserDetailService implements UserDetailsService {
+
+
+    public final MyUserService service;
+
+    public final PasswordEncoder passwordEncoder;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+           MyUser user = service.findByUser(username);
+           return User.builder().username(user.getUsername()).password(passwordEncoder.encode(user.getUsername())).roles(user.getRole()).build();
+    }
+}
---
+++ client-app/src/main/java/com/example/clientapp/security/service/MyUserService.java
@@ -0,0 +1,19 @@
+package com.example.clientapp.security.service;
+
+import com.example.clientapp.security.model.MyUser;
+import com.example.clientapp.security.repository.MyUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MyUserService {
+
+    private final MyUserRepository myUserRepository;
+
+    public MyUser findByUser(String username) throws UsernameNotFoundException {
+        return myUserRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+}
+
