0c2a8355c5830373f0537b6c61e304ac1ed04082
Mr.D <danil36rusdanil@mail.ru>
2024-06-10 17:00:42 +0300

    add config

--- client-app/src/main/java/com/example/clientapp/security/config/SecurityConfiguration.java
+++ client-app/src/main/java/com/example/clientapp/security/config/SecurityConfiguration.java
@@ -1,9 +1,32 @@
 package com.example.clientapp.security.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWeb
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests( authorize ->{
+            authorize.requestMatchers("/registration").permitAll();
+            authorize.anyRequest().authenticated();
+        })
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
